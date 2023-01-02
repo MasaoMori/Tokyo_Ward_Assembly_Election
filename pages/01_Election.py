523f03354d153e82ec5e8ffd5962832ae1c7f082
Masao Mori <mori@irds.titech.ac.jp>
2023-01-02 15:29:06 +0900

    3rd ver

--- pages/01_Election.py
+++ pages/01_Election.py
@@ -13,7 +13,8 @@ ppath = str(pathlib.Path(cpath))
 
 #####
 parties = pd.read_csv(ppath + '/data/parties.csv')
-ward_master = pd.read_csv(ppath + '/data/ward_master.csv')
+#ward_master = pd.read_csv(ppath + '/data/ward_master.csv')
+tkcode = pd.read_csv(ppath + '/data/tkcode.csv')
 summary = pd.read_csv(ppath + '/data/summary.csv')
 data = pd.read_csv(ppath + '/data/election.csv',
                    dtype={'得票数':float,
@@ -22,32 +23,32 @@ data = pd.read_csv(ppath + '/data/election.csv',
 #####
 
 st.set_page_config(layout="wide")
+
 #### サイドバーでパラメータを決める
-wardlist = ['中央', '台東', '品川', '目黒', '大田', '渋谷']
-yearlist = ('2019','2015','2011')
+wardlist = list(tkcode[(131000 < tkcode['自治体コード']) & (tkcode['自治体コード'] < 132000)]['区名'])
+wardlist = list(map((lambda x: x.replace('区','')),wardlist))
+
 wardname = st.sidebar.selectbox("対象区名", wardlist)
 obj_value = st.sidebar.radio('実数か割合を選んでください',('得票数','得票率%'))
 
- #year = st.sidebar.selectbox('年',yearlist)
+# year = st.sidebar.selectbox('年',yearlist)
+sum_tmp = summary[summary['区名']==wardname]
+yearlist = list(map((lambda x: str(x)),list(sum_tmp.sort_values('年', ascending=False)['年'])))
+
 year = yearlist[0]
-#　year = st.sidebar.selectbox('年',(2011,2015,2019))
 
 
 pivot_columns = ['年','区名','氏名','得票数','得票率%','略称党名','自治体コード']
 
 m = data[(data['当落']=='当') & (data['区名']==wardname)].loc[:,pivot_columns]
 
-# m = pd.concat([tmp,tmp['氏名'] + '('+tmp['略称党名']+ ')'],axis=1).rename(columns={0:'候補者'})
-# これはやめ
 
 # 凡例の出現順序を決める
-#sorted_candidate = list(m[m['年']==year].sort_values('得票数',ascending=False)['氏名'])
 sorted_candidate = list(m[m['年']==year].sort_values(obj_value, ascending=False)['氏名'])
 
 #y_upper = np.ceil(max(m[m['年']==year]['得票数'])*1.3)
 #y_lower = np.ceil(min(m[m['年']==year]['得票数'])*0.7)
 # 縦軸の上限下限
-
 
 
 import plotly.express as px
@@ -58,27 +59,34 @@ fig = px.line(
     text=obj_value,
     color='氏名',
     category_orders={'氏名':sorted_candidate,
-                     '年':['2011','2015','2019']},
+                     '年':yearlist},
     markers=True,
     height=1200,
     width=600
 )
 
-#fig.update_layout(xaxis=dict(range=(2010,2020),dtick=1),
-#                  yaxis=dict(range=(y_lower,y_upper),dtick=1000))
 
 mm = m.pivot(index=['氏名'],columns='年',values=obj_value).sort_values(year,ascending=False).reset_index()
 mm.style.applymap("{:,.0}")
 
 #
 # Plot procedure
+#
 
 st.title(wardname+'区')
 st.subheader('選挙概要')
-AgGrid.AgGrid(summary[summary['区名']==wardname],fit_columns_on_grid_load=True,height=120)
+
+show_summary = sum_tmp[['年','区名','定数','候補者数','有権者数','投票率%','投票者数']]
+
+AgGrid.AgGrid(show_summary, fit_columns_on_grid_load=True,height=120)
+
+
 st.subheader(obj_value + 'グラフ')
 st.write("凡例は最新の選挙結果を降順に並べています。データは当選者のみです。凡例の候補者名をダブルクリックすると、グラフ全体が消え、その候補者だけを表示できます。その状態で、他の候補者をシングルクリックすると、その候補者のグラフが出現します。")
+
 st.plotly_chart(fig, use_container_width=True)
+
+
 st.subheader(obj_value + '表')
 st.write("最新の選挙結果を降順に並べています。データは当選者のみです。ヘッダの年をクリックすると、その年のデータで並び替えられます。空欄は実績がないことを意味します。")
 AgGrid.AgGrid(mm,fit_columns_on_grid_load=True,height=600)
