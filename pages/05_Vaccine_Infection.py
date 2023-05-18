5edec907f0a20ad9165b94a7fb6ee8065ada0789
mori <mori@irds.titech.ac.jp>
2023-05-18 22:15:47 +0900

    20230518-6

--- pages/05_Vaccine_Infection.py
+++ pages/05_Vaccine_Infection.py
@@ -4,37 +4,6 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-cpath = os.getcwd()
-ppath = str(pathlib.Path(cpath))
-
-#####
-vc = pd.read_csv(ppath + '/data/vc.csv')
-pref = pd.read_csv(ppath + '/data/pref.csv')
-
-vc = vc[(vc['日付']>='2021/4/1')&(vc['日付']<='2023/4/30')]
-
-gtitle =  '接種者と新規陽性者 2021/4 ~ 2023/4'
-st.subheader(gtitle)
-w = st.sidebar.selectbox('都道府県名',list(pref['都道府県名']))
-
-def distp(p):
-    ac=vc[(vc['都道府県名']==p)]
-    
-    fig=px.line(ac, x='日付', y='人数', color='カテゴリ',
-                width=1400,height=800, title=p+gtitle)
-    fig.update_xaxes(dtick="M1",tickformat="%m\n%Y")
-#    fig.update_yaxes(tickformat=',')
-    fig.add_annotation(x='2021-12-1', y=0,text="第3回接種開始",showarrow=True, ay=30)
-    fig.add_annotation(x='2022-5-27', y=0,text="第4回接種開始",showarrow=True, ay=30)
-    fig.add_annotation(x='2022-9-30', y=0,text="第5回接種開始",showarrow=True, ay=30)
-    fig.update_layout(legend=dict(
-        yanchor="top",
-        y=0.99,
-        xanchor="left",
-        x=0.01
-    ))
-    return fig
-
 text="""
 ## 情報源
 ### ワクチン接種者（1〜6回目接種者全て）
@@ -54,8 +23,35 @@ text="""
 
 接種者の分布と新規陽性者の分布（山のある時期）がほぼ同期していることに注意。ワクチン接種後、しばらくすると免疫力がさがることがあるようだ。
 """
+
+cpath = os.getcwd()
+ppath = str(pathlib.Path(cpath))
+
+vc = pd.read_csv(ppath + '/data/vc.csv')
+pref = pd.read_csv(ppath + '/data/pref.csv')
+
+vc = vc[(vc['日付']>='2021/4/1')&(vc['日付']<='2023/4/30')]
+
+gtitle =  '接種者と新規陽性者 2021/4 ~ 2023/4'
+st.subheader(gtitle)
+w = st.sidebar.selectbox('都道府県名',list(pref['都道府県名']))
+
+fig=px.line(vc[vc['都道府県名']==w], x='日付', y='人数', color='カテゴリ',
+            width=1400,height=800, title=w+'の'+gtitle)
+fig.update_xaxes(dtick="M1",tickformat="%b\n%Y")
+#    fig.update_yaxes(tickformat=',')
+fig.add_annotation(x='2021-12-1', y=0,text="第3回接種開始",showarrow=True, ay=30)
+fig.add_annotation(x='2022-5-27', y=0,text="第4回接種開始",showarrow=True, ay=30)
+fig.add_annotation(x='2022-9-30', y=0,text="第5回接種開始",showarrow=True, ay=30)
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="left",
+    x=0.01
+))
+
 st.markdown(text)
-st.plotly_chart(distp(w), use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
 
 
 
